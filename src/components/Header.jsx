1d7268e341cc6a812bae09268126c1f04d82a824
Logistis <leandrofigueiredofranco@gmail.com.br>
2023-01-25 13:18:26 -0300

    requisito 3 e 4

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
+
+export default class Header extends Component {
+  state = { loadingStatus: true, UserName: '' };
+
+  componentDidMount() {
+    this.renderName();
+  }
+
+  renderName = async () => {
+    const user = await getUser();
+    this.setState({ loadingStatus: false, UserName: user.name });
+  };
+
+  render() {
+    const { loadingStatus, UserName } = this.state;
+    console.log(loadingStatus);
+    if (loadingStatus) {
+      return (
+        <header>
+          <Loading />
+        </header>
+      );
+    }
+    return (
+      <header testid="header-component">
+        <h3>
+          { UserName }
+        </h3>
+        <Link data-testid="link-to-search" to="/search">Search</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+      </header>
+    );
+  }
+}
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class album extends Component {
   render() {
     return (
-      <div data-testid="page-album">album</div>
+      <div data-testid="page-album">
+        <Header />
+      </div>
     );
   }
 }
